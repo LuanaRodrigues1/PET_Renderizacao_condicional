1a683d599dedeba87300ff8312c2680d39a4f11e
LuanaRodrigues1 <marialuana2810@gmail.com>
2024-01-05 15:26:30 -0300

    finish

--- app/src/app/Postagem.tsx
+++ app/src/app/Postagem.tsx
@@ -1,25 +1,46 @@
 import { SlOptions } from "react-icons/sl";
 import { FaUserCircle } from "react-icons/fa";
+
+import { FaHeart } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
+
+import { FaRegComment } from "react-icons/fa";
+import { BsSend } from "react-icons/bs";
+import { FaRegBookmark } from "react-icons/fa";
+
 import "./icones.css";
 
 interface propsPostagem {
   username: string;
   location: string;
   profile_picture: string;
+  post: string;
+  like: boolean;
+  describion: string;
+  hastags: string;
+  quantLikes: number;
 }
 
 export default function Postagem({
   username,
   location,
   profile_picture,
+  post,
+  like,
+  describion,
+  hastags,
+  quantLikes,
 }: propsPostagem) {
   return (
     <>
-      <div className="border-solid border-2 border-black mx-96 my-8">
-        <div className="flex justify-between items-center m-10">
+      <div className="border-solid border-2 border-black mx-500 my-8 py-10 pr-20 pl-20">
+        <div className="flex justify-between items-center mb-12">
           <div className="flex items-center">
-            <FaUserCircle className="icon-large" />
-            {/*<img src={profile_picture} alt="Foto de perfil" />*/}
+            <img
+              src={profile_picture}
+              alt="Foto de perfil"
+              className="rounded-full"
+            />
             <div className="ml-4">
               <h1 className="font-bold text-2xl">{username}</h1>
               <h4>{location}</h4>
@@ -28,7 +49,38 @@ export default function Postagem({
           <SlOptions className="icon-medium" />
         </div>
 
-        <img src="" alt="" />
+        <div className="flex justify-center items-center">
+          <img src={post} alt="Post" className="h-800 w-800" />
+        </div>
+
+        <div className="flex justify-between pt-12 mb-5">
+          <div className="flex">
+            {like === true ? (
+              <FaHeart className="text-red-500 mr-5 h-12 w-12" />
+            ) : (
+              <FaRegHeart className="mr-5 h-12 w-12" />
+            )}
+
+            <FaRegComment className="mr-5 h-12 w-12" />
+            <BsSend className="h-12 w-12" />
+          </div>
+
+          <FaRegBookmark className="h-12 w-12" />
+        </div>
+
+        <div>
+          <div className="flex items-center mb-5">
+            <FaHeart className="mr-3" />
+            <strong>{quantLikes} likes</strong>
+          </div>
+          <div className="flex mb-2">
+            <strong>{username}</strong>
+            <h3 className="ml-2">{describion}</h3>
+          </div>
+          <a href="./" className="text-blue-800">
+            {hastags}
+          </a>
+        </div>
       </div>
     </>
   );
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -5,9 +5,36 @@ export default function Home() {
   return (
     <>
       <Postagem
-        username={"LuanaRodrigues"}
-        location={"Porto de Galinhas"}
-        profile_picture=""
+        username={"JoaoSilva"}
+        location={"Rio de Janeiro"}
+        profile_picture="./user4.png"
+        post="./picture1.png"
+        like={false}
+        describion="Passeio maravilhoso à beira-mar."
+        hastags={"#verao #sol #praia #paz"}
+        quantLikes={94}
+      />
+
+      <Postagem
+        username={"MariaSouza"}
+        location={"Montanhas Azuis"}
+        profile_picture="./user2.png"
+        post="./picture2.png"
+        like={true}
+        describion="Explorando as belas trilhas das montanhas."
+        hastags={"#natureza #aventura #neve"}
+        quantLikes={137}
+      />
+
+      <Postagem
+        username={"AnaCarvalho"}
+        location={"Vila do Campo"}
+        profile_picture="./user3.png"
+        post="./picture3.png"
+        like={true}
+        describion="Saboreando um delicioso café."
+        hastags={"#vidaSimple #tranquilidade"}
+        quantLikes={87}
       />
     </>
   );
